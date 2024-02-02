e0235891d4840ed60a0920f5cfc87be5abcf1e23
Tomas Santana <tomas.30604530@uru.edu>
2024-02-01 20:17:03 -0400

    AAAA

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <button @click="count++">
+            count is: {{ count }}
+        </button>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+    import { ref } from 'vue'
+    import 
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    const count = ref(0)
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/global/sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+    <aside>
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/">Home</router-link>
+                </li>
+                <li>
+                    <router-link to="/about">About</router-link>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+</template>
+
+<!-- no se soy idiota no entiendo nada -->
+
+
+    
